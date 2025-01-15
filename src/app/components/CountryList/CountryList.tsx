ba4fdc16b9de44136d5850243635e370e42e1210
Dmitriy Yakovets <dmitriy.yakovets@gmail.com>
2025-01-15 22:58:18 +0300

    Сохранение прогресса

--- src/app/components/CountryList/CountryList.tsx
+++ src/app/components/CountryList/CountryList.tsx
@@ -1,9 +1,36 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
 
-import { FC, useRef, useState } from "react";
+import { CSSProperties, FC, useEffect, useState } from "react";
+import { TransitionStatus } from "react-transition-group";
 import { CountriesDTO } from "../../lib/types";
 import { CountryListItem } from "./CountryListItem";
+
+const duration1 = 500;
+
+const defaultStyle1 = {
+  transition: `transform ${duration1}ms ease-in-out`,
+};
+
+const transitionStyles1: Partial<Record<TransitionStatus, CSSProperties>> = {
+  entering: { transform: "translateX(100%)" },
+  entered: { transform: "translateX(100%)" },
+  exiting: { transform: "none" },
+  exited: { transform: "none" },
+};
+
+const duration2 = 800;
+
+const defaultStyle2 = {
+  transition: `transform ${duration2}ms ease-in-out`,
+};
+
+const transitionStyles2: Partial<Record<TransitionStatus, CSSProperties>> = {
+  entering: { transform: "translateY(-32px)" },
+  entered: { transform: "translateY(-32px)" },
+  exiting: { transform: "none" },
+  exited: { transform: "none" },
+};
 
 type Props = {
   countryList: CountriesDTO;
@@ -11,23 +38,38 @@ type Props = {
 
 export const CountryList: FC<Props> = ({ countryList }) => {
   const [countries, setCountries] = useState(countryList);
-  const deletedIndexRef = useRef(countryList.length);
+  const [deletedIndex, setDeletedIndex] = useState(-1);
+  const [finished, setFinished] = useState(false);
 
-  const handleClick = (index: number): void => {
-    deletedIndexRef.current = index;
-    const newList = countries.filter((_country, i) => i !== index);
-    setCountries(newList);
-    console.log("Debug ~ deletedIndex:", deletedIndexRef.current);
-  };
+  // const handleClick = (): void => {
+  useEffect(() => {
+    if (finished) {
+      const newList = countries.filter((_country, i) => i !== deletedIndex);
+      console.log("Debug ~ newList:", newList.length);
+      setCountries(newList);
+      setDeletedIndex(-1);
+      setFinished(false);
+      // console.log("Debug ~ deletedIndex:", deletedIndexRef.current);
+    }
+  }, [finished]);
 
   return (
     <ol className="mx-auto px-2 max-w-[700px] overflow-hidden">
       {countries.map((country, i) => (
         <CountryListItem
           key={country.iso_code3}
+          defaultStyle={i > deletedIndex ? defaultStyle2 : defaultStyle1}
+          transitionStyles={
+            i > deletedIndex ? transitionStyles2 : transitionStyles1
+          }
           country={country}
-          inPropUp={i >= deletedIndexRef.current}
-          handleClick={() => handleClick(i)}
+          countryIndex={i}
+          deletedIndex={deletedIndex}
+          finished={finished}
+          duration={i > deletedIndex ? duration2 : duration1}
+          setFinished={() => setFinished(true)}
+          setDeletedIndex={() => setDeletedIndex(i)}
+          // handleClick={handleClick}
         />
       ))}
     </ol>
--- src/app/components/CountryList/CountryListItem.tsx
+++ src/app/components/CountryList/CountryListItem.tsx
@@ -1,85 +1,125 @@
+"use client";
+
 import { CountryDTO } from "@/app/lib/types";
-import { CSSProperties, FC, useRef, useState } from "react";
+import Image from "next/image";
+import { CSSProperties, FC, useRef } from "react";
 import { Transition } from "react-transition-group";
 import { TransitionStatus } from "react-transition-group/Transition";
 import { Button } from "../Button";
 
-const duration = 700;
+// const duration = 1000;
 
 // const defaultStyle = {
 //   transition: `transform ${duration}ms ease-in-out`,
-//   transform: "translateX(0)",
 // };
 
 // const transitionStyles: Partial<Record<TransitionStatus, CSSProperties>> = {
-//   entering: { transform: "translateX(100%)" },
+//   entering: { transform: "none" },
+//   entered: { transform: "translateX(100%)" },
+//   exiting: { transform: "none" },
+//   exited: { transform: "translateY(32px)" },
+// };
+
+// const duration1 = 600;
+
+// const defaultStyle1 = {
+//   transition: `transformX ${duration1}ms ease-in-out`,
+// };
+
+// const transitionStyles1: Partial<Record<TransitionStatus, CSSProperties>> = {
+//   entering: { transform: "none" },
 //   entered: { transform: "translateX(100%)" },
 // };
 
-const defaultStyle1 = {
-  transition: `transformX ${duration}ms ease-in-out`,
-};
+// const duration2 = 600;
 
-const transitionStyles1: Partial<Record<TransitionStatus, CSSProperties>> = {
-  entering: { transform: "translateX(100%)" },
-  entered: { transform: "translateX(100%)" },
-};
+// const defaultStyle2 = {
+//   transition: `transformY ${duration2}ms ease-in-out`,
+// };
 
-const defaultStyle2 = {
-  transition: `transformY ${duration}ms ease-in-out`,
-};
+// const transitionStyles2: Partial<Record<TransitionStatus, CSSProperties>> = {
+//   entering: { transform: "none" },
+//   entered: { transform: "translateY(-32px)" },
+// };
 
-const transitionStyles2: Partial<Record<TransitionStatus, CSSProperties>> = {
-  entering: { transform: "translateY(-100%)" },
-  entered: { transform: "translateY(-100%)" },
-  // exiting: { transform: "translateY(-100%)" },
-  // exited: { transform: "translateY(-100%)" },
+type DefaultStyle = {
+  transition: string;
 };
 
 type Props = {
+  defaultStyle: DefaultStyle;
+  transitionStyles: Partial<Record<TransitionStatus, CSSProperties>>;
   country: CountryDTO;
-  inPropUp: boolean;
-  handleClick: () => void;
+  countryIndex: number;
+  deletedIndex: number;
+  finished: boolean;
+  duration: number;
+  setFinished: () => void;
+  setDeletedIndex: () => void;
+  // handleClick: () => void;
 };
 
 export const CountryListItem: FC<Props> = ({
+  defaultStyle,
+  transitionStyles,
   country,
-  inPropUp,
-  handleClick,
+  countryIndex,
+  deletedIndex,
+  finished,
+  duration,
+  setFinished,
+  setDeletedIndex,
+  // handleClick,
 }) => {
-  // console.log("Debug ~ inPropUp:", inPropUp);
   const nodeRef = useRef(null);
-  const [inPropDelete, setInPropDelete] = useState(false);
-  const defaultStyle = inPropUp ? defaultStyle2 : defaultStyle1;
-  const transitionStyles = inPropUp ? transitionStyles2 : transitionStyles1;
+  // const [inPropDelete, setInPropDelete] = useState(false);
+
+  // const currentTransition = (): [
+  //   DefaultStyle,
+  //   Partial<Record<TransitionStatus, CSSProperties>>,
+  //   number,
+  // ] => {
+  //   if (countryIndex > deletedIndex)
+  //     return [defaultStyle2, transitionStyles2, duration2];
+
+  //   return [defaultStyle1, transitionStyles1, duration1];
+  // };
+
+  // const [defaultStyle, transitionStyles, duration] = currentTransition();
+  // console.log("Debug ~ duration:", duration);
+  // console.log("Debug ~ transitionStyles:", transitionStyles);
+  // console.log("Debug ~ defaultStyle:", defaultStyle);
 
   return (
     <Transition
       nodeRef={nodeRef}
-      in={inPropDelete || inPropUp}
+      in={deletedIndex !== -1 && countryIndex >= deletedIndex}
       timeout={duration}
-      {...(inPropDelete && { onEntered: handleClick })}
+      onEntered={setFinished}
     >
       {(state) => (
         <li
           key={country.iso_code3}
           ref={nodeRef}
-          className="grid grid-cols-[80px_100px_480px_20px] mb-2"
+          className="grid grid-cols-[80px_100px_480px_20px] items-center mb-2"
           style={{
             ...defaultStyle,
             ...transitionStyles[state],
           }}
         >
           {country.flag_url ? (
-            <div className="flex items-center">
-              <img src={`https:${country.flag_url}`} alt={country.iso_code3} />
-            </div>
+            <Image
+              src={`https:${country.flag_url}`}
+              alt=""
+              width={22}
+              height={14}
+            />
           ) : (
             <div className="w-9 h-6" />
           )}
           <p className="w-10 ml-1">{country.iso_code3}</p>
           <p className="w-[424px]">{country.name_ru}</p>
-          <Button handleClick={() => setInPropDelete(true)} />
+          <Button handleClick={() => setDeletedIndex()} />
         </li>
       )}
     </Transition>
